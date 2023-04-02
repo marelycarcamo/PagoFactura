1edfa60f8a4f80c7c36ac78b75d502c7a2a6c393
marelycarcamo <44790921+marelycarcamo@users.noreply.github.com>
2023-04-02 17:38:04 -0400

    version 1

---
+++ src/pagoFactura/PagoFactura.java
@@ -0,0 +1,26 @@
+/*
+ * ESTE PROGRAMA REALIZA EL CALCULO DE FACTURA
+ * @MARELY
+ */
+package pagoFactura;
+
+import java.util.Scanner;
+
+public class PagoFactura {
+	private static Scanner producto;
+
+	public static void main(String[] args) {
+		// declaración de variables
+		float neto;
+		float bruto;
+		float iva = 0.19f;
+		//lectura de datos
+		producto = new Scanner(System.in);
+		System.out.println("Ingrese valor del producto: ");
+		neto = producto.nextFloat();
+		// calculo del iva
+		bruto = (neto * iva) + neto;
+		// despliegue de resultado
+		System.out.println("El resultado es: " + bruto);
+	}
+}
